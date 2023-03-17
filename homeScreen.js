cf241c0cc387779094119caed5475d0a564908e8
shivamvaishkiyar <104146996+shivamvaishkiyar@users.noreply.github.com>
2023-03-17 11:38:30 +0530

    Add files via upload

---
+++ homeScreen.js
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+export default class HomeScreen extends Component{
+  render(){
+    return(
+      <View>
+        <Text>HomeScreen</Text>
+      </View>
+    )
+  }
+}
